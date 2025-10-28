0452f3fd1b6c378e6e1f247c58d1d1cecc254980
NesrineYAH <nesrineyahoum@gmail.com>
2025-10-28 14:29:40 +0100

    Générer les données clients

--- Scripts/marketing_pipeline.py
+++ Scripts/marketing_pipeline.py
@@ -9,5 +9,17 @@ def generate_marketing_data():
 # Données clients 
     #Initialise une liste de clients vide
     clients = []
+    
+      #- Boucle qui s’exécute 1000 fois pour créer 1000 clients.
+    for i in range(1000):
+        clients.append({
+            'client_id': f"C_{i:04d}",         #Identifiant unique du client avec 4 chiffres,
+            'age': np.random.randint(18, 65),    #Âge aléatoire entre 18 et 65,
+            'ville': fake.city(),            #Nom de la ville généré aléatoirement,
+            'segment': np.random.choice(['Premium', 'Standard', 'Basique']),       #Segment choisi aléatoirement parmi trois options,
+            'canal_acquisition': np.random.choice(['Partenariat', 'SEO', 'Publicité', 'Affiliation', 'Email', 'Direct'])    #Canaux d’acquisition choisi logiquement pour attirer les clients
+        })
+    
 
- 
+    # Données interactions   
+    interactions = []
